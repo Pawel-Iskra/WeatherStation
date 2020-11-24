7ae9721e39de5f1232f4dcd262d9107dce63ca78
Pawel-Iskra <paweliszcz@wp.pl>
2020-11-24 13:27:15 +0100

    [WeatherStation:master] initial commit of the project

---
+++ src/test/java/com/weatherStation/WeatherStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
